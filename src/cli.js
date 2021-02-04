21ec425c58d41bdadfcf9c3bd735966b3f1211e0
felix <11788526+flxsu@users.noreply.github.com>
2021-02-04 04:25:07 +0100

    before rewrite

--- src/cli.js
+++ src/cli.js
@@ -1,71 +1,140 @@
 #!/usr/bin/env node
 
-const { process_file } = require('./main.js')
+// console.log(process.argv)
+
+// Imports
+const { create_output } = require('./main.js')
 const { extname } = require('path')
 const chokidar = require('chokidar')
-const { stat } = require('fs')
 
-function has_html_format (arg) {
-	return arg && extname(arg) === '.html'
+// Argument format predicates
+const has_html_format = (arg) => arg && extname(arg) === '.html'
+const has_folder_format = (arg) => arg && extname(arg) === ''
+
+// Option, predicate couples
+const [ arg_input, arg_output, arg_watch ] = [ '-i', '-o', '-w' ]
+const option_predicates = {
+	[arg_input]: has_html_format,
+	[arg_output]: has_html_format,
+	[arg_watch]: has_folder_format
 }
 
-function has_folder_format (arg) {
-	return arg && extname(arg) === ''
+// Build argument object out of raw argument array
+const build_arg_object = (_args) => {
+	const args = {}
+	let idx = 2
+	while (idx < _args.length) {
+		if (idx % 2 === 0) {
+			const [ option, arg ] = [ _args[idx], _args[idx + 1] ]
+			if (!arg) {
+				throw new Error(`No argument provided for option "${option}"`)
+			}
+			args[option] = arg
+		}
+		idx += 2
+	}
+	return args
 }
 
-function initialize () {
-	let path_in
-	let path_out
-	let path_watcher
-	let is_output_arg = false
-	let is_watcher_arg = false
-	for (const arg of process.argv.slice(2)) {
-		if (arg === '-o') {
-			is_output_arg = true
+// Verify argument object for correctness
+const verify_arg_object = (args) => {
+	for (const [ option, arg ] of Object.entries(args)) {
+		const predicate = option_predicates[option]
+		if (!predicate) {
+			throw new Error(`Invalid option: "${option}"`)
 		}
-		else if (arg === '-w') {
-			is_watcher_arg = true
-		}
-		else {
-			if (is_watcher_arg) {
-				if (!has_folder_format(arg)) {
-					throw new Error(`Argument isn't a folder: "${arg}"`)
-				}
-				path_watcher = arg
-				is_watcher_arg = false
-			}
-			else {
-				if (!has_html_format(arg)) {
-					throw new Error(`Argument isn't an HTML file: "${arg}"`)
-				}
-				if (is_output_arg) {
-					path_out = arg
-					is_output_arg = false
-				}
-				else {
-					path_in = arg
-				}
-			}
+		if (!predicate(arg)) {
+			throw new Error(`Invalid argument for option "${option}": "${arg}"`)
 		}
 	}
+}
+
+try {
+	// Build argument object
+	const args = build_arg_object(process.argv)
+	// Verify argument object, throw if not correct
+	verify_arg_object(args)
+	// Arguments
+	const path_in = args[arg_input]
+	const path_out = args[arg_output]
+	const path_watch = args[arg_watch]
+
+	// Require at least input and output paths
 	if (!(path_in && path_out)) {
 		throw new Error('Insufficient arguments')
 	}
 
-	if (path_watcher) {
-		const watcher = chokidar.watch(path_watcher, {
+	// If watcher path given, watch that directory for changes
+	if (path_watch) {
+		const watcher = chokidar.watch(path_watch, {
 			ignored: /(^|[\/\\])\../, // ignore dotfiles
 			persistent: true
 		})
 		const changeEvents = [ 'add', 'change', 'unlink', 'addDir', 'unlinkDir' ]
-		// on change
+		// On change, recreate the HTML output
 		for (const eventType of changeEvents) {
-			watcher.on(eventType, () => process_file(path_in, path_out))
+			watcher.on(eventType, () => create_output(path_in, path_out))
 		}
 	}
-	return [ path_in, path_out ]
+
+	// Create HTML output
+	create_output(path_in, path_out)
+} catch (error) {
+	console.error(error)
 }
 
-const paths = initialize()
+// function initialize () {
+// 	let path_in
+// 	let path_out
+// 	let path_watcher
+// 	let is_output_arg = false
+// 	let is_watcher_arg = false
+// 	for (const arg of process.argv.slice(2)) {
+// 		if (arg === '-o') {
+// 			is_output_arg = true
+// 		}
+// 		else if (arg === '-w') {
+// 			is_watcher_arg = true
+// 		}
+// 		else {
+// 			if (is_watcher_arg) {
+// 				if (!has_folder_format(arg)) {
+// 					throw new Error(`Argument isn't a folder: "${arg}"`)
+// 				}
+// 				path_watcher = arg
+// 				is_watcher_arg = false
+// 			}
+// 			else {
+// 				if (!has_html_format(arg)) {
+// 					throw new Error(`Argument isn't an HTML file: "${arg}"`)
+// 				}
+// 				if (is_output_arg) {
+// 					path_out = arg
+// 					is_output_arg = false
+// 				}
+// 				else {
+// 					path_in = arg
+// 				}
+// 			}
+// 		}
+// 	}
+// 	if (!(path_in && path_out)) {
+// 		console.log(path_in, path_out)
+// 		throw new Error('Insufficient arguments')
+// 	}
 
-process_file(paths[0], paths[1])
+// 	if (path_watcher) {
+// 		const watcher = chokidar.watch(path_watcher, {
+// 			ignored: /(^|[\/\\])\../, // ignore dotfiles
+// 			persistent: true
+// 		})
+// 		const changeEvents = [ 'add', 'change', 'unlink', 'addDir', 'unlinkDir' ]
+// 		// on change
+// 		for (const eventType of changeEvents) {
+// 			watcher.on(eventType, () => create_output(path_in, path_out))
+// 		}
+// 	}
+// 	create_output(path_in, path_out)
+// }
+
+// initialize()
--- src/main.js
+++ src/main.js
@@ -3,100 +3,67 @@ const { dirname, sep } = require('path')
 
 const DEFAULT_LINEBREAK = process.platform === 'win32' ? '\r\n' : '\n'
 const REGEX_CURLY = /({{)([\s\S][^{{]+)(}})/g
-const REGEX_INSERT = /(?:(?:(\d)\s*\*\s*)?((?:\w|\/)+\.html))(\n|(?:\r\n))?/g
+const REGEX_INSERT = /(\s*)(?:(?:(\d)\s*\*\s*)?((?:\w|\/)+\.html))(\n|(?:\r\n))?/g
 
 function read_file (path) {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf8', (err, data) => {
-            if (err) reject(err)
-            resolve(data)
-        })
-    })
+	return new Promise((resolve, reject) => {
+		readFile(path, 'utf8', (err, data) => {
+			if (err) reject(err)
+			resolve(data)
+		})
+	})
 }
 
+// function really_normalize_path (path) {
+// 	const
+// }
+
+async function EVOLVE (entry) {}
+
 async function resolve (filepath) {
-    let data = await read_file(filepath)
-    let match_curly
-    while ((match_curly = REGEX_CURLY.exec(data))) {
-        const length = match_curly[0].length
-        let inserts = match_curly[2]
-        let match_insert
-        while ((match_insert = REGEX_INSERT.exec(inserts))) {
-            const length = match_insert[0].length
-            const multiplier = Number(match_insert[1])
-            const has_multiplier = !isNaN(multiplier)
-            const html_path = dirname(filepath) + sep + match_insert[2]
-            const linebreak_character = match_insert[3] || DEFAULT_LINEBREAK
-            let html_data = await resolve(dirname(filepath) + sep + html_path)
-            if (has_multiplier) {
-                let counter = multiplier
-                while (counter > 1) {
-                    html_data += linebreak_character + html_data
-                }
-            }
-            const previous = inserts.slice(0, match_insert.index)
-            const next = inserts.slice(match_insert.index + length)
-            const possible_linebreak =
-                next.trim() === '' ? '' : linebreak_character
-            inserts = previous + html_data + possible_linebreak + next
-        }
-        const previous = data.slice(0, match_curly.index)
-        const next = data.slice(match_curly.index + length)
-        data = previous + inserts + next
-    }
-    return data
+	let data = await read_file(filepath)
+	let match_curly
+	while ((match_curly = REGEX_CURLY.exec(data))) {
+		const length = match_curly[0].length
+		let inserts = match_curly[2]
+		let match_insert
+		while ((match_insert = REGEX_INSERT.exec(inserts))) {
+			const length = match_insert[0].length
+			const indent = match_insert[1]
+			const multiplier = Number(match_insert[2])
+			const has_multiplier = !isNaN(multiplier)
+			const html_path = dirname(filepath) + sep + match_insert[3]
+			const linebreak_character = match_insert[4] || DEFAULT_LINEBREAK
+			console.log(filepath)
+			console.log(html_path)
+			let html_data = await resolve(html_path)
+			if (has_multiplier) {
+				let counter = multiplier
+				while (counter > 1) {
+					html_data += linebreak_character + html_data
+				}
+			}
+			const previous = inserts.slice(0, match_insert.index)
+			const next = inserts.slice(match_insert.index + length)
+			const possible_linebreak = next.trim() === '' ? '' : linebreak_character
+			inserts = previous + html_data + possible_linebreak + next
+		}
+		const previous = data.slice(0, match_curly.index)
+		const next = data.slice(match_curly.index + length)
+		data = previous + inserts + next
+	}
+	return data
 }
 
 async function create_output (path_in, path_out) {
-    try {
-        const data = await resolve(path_in)
-        writeFile(path_out, data, {}, (err) => {
-            if (err) throw err
-        })
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-async function process_file (path_in, path_out) {
-    const curly_regex = /({{)([\s\S][^{{]+)(}})/g
-    const insert_regex = /(?:(?:(\d)\s*\*\s*)?((?:\w|\/)+\.html))(\n|(?:\r\n))?/g
-    try {
-        let data = await read_file(path_in)
-        let curly_match
-        while ((curly_match = curly_regex.exec(data))) {
-            const length = curly_match[0].length
-            let inserts = curly_match[2]
-            let insert_match
-            while ((insert_match = insert_regex.exec(inserts))) {
-                const length = insert_match[0].length
-                const multiplier = Number(insert_match[1])
-                const has_multiplier = !isNaN(multiplier)
-                const html_path = dirname(path_in) + sep + insert_match[2]
-                const linebreak_character = insert_match[3] || DEFAULT_LINEBREAK
-                let html_data = await read_file(html_path)
-                if (has_multiplier) {
-                    let counter = multiplier
-                    while (counter > 1) {
-                        html_data += linebreak_character + html_data
-                    }
-                }
-                const previous = inserts.slice(0, insert_match.index)
-                const next = inserts.slice(insert_match.index + length)
-                const possible_linebreak =
-                    next.trim() === '' ? '' : linebreak_character
-                inserts = previous + html_data + possible_linebreak + next
-            }
-            const previous = data.slice(0, curly_match.index)
-            const next = data.slice(curly_match.index + length)
-            data = previous + inserts + next
-        }
-        writeFile(path_out, data, {}, (err) => {
-            if (err) throw err
-        })
-    } catch (error) {
-        console.error(error)
-    }
+	try {
+		const data = await resolve(path_in)
+		writeFile(path_out, data, {}, (err) => {
+			if (err) throw err
+		})
+	} catch (error) {
+		console.error(error)
+	}
 }
 
 module.exports = { create_output }
